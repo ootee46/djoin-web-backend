5e6a495b3ead761163bcb3a3dd1bb99b66e9eea0
wichian <wichian@gmail.com>
2025-09-29 20:19:43 +0700

    Restore to fergory

--- src/app/app.resolvers.ts
+++ src/app/app.resolvers.ts
@@ -15,9 +15,10 @@ export class InitialDataResolver implements Resolve<any>
      * Constructor
      */
     constructor(
-        private  readonly _navigationService: NavigationService,
-        private  readonly _userService: UserService,
-        private  readonly _dashboardService: DashboardService,
+        private _navigationService: NavigationService,
+        private _authService: AuthService,
+        private _userService: UserService,
+        private _dashboardService: DashboardService,
     )
     {
     }
@@ -37,6 +38,7 @@ export class InitialDataResolver implements Resolve<any>
         // Fork join multiple API endpoint calls to wait all of them to finish
         return forkJoin([
             this._navigationService.get(),
+            this._authService.getAntiforgery(),
             this._dashboardService.getPackageInfo(),
             this._userService.get()
         ]);
